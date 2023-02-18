a3b3d9edc7cefb31fd2982e1b8c6f0ab569e78e7
Bantikevat199 <bantikewat199@gmail.com>
2023-02-18 12:07:55 +0530

    add lambda function

---
+++ lambda/lambda1.py
@@ -0,0 +1,26 @@
+#simple lambda function
+#lambda function
+square = lambda a: a*a
+#call lambda function
+result = square(6)
+print(result)
+
+
+
+#Lambda Function – Multiple arguments
+#lambda function
+mul = lambda a,b: a*b
+#call lambda function
+result = mul(5,3)
+print(result)
+
+
+
+
+
+#Lambda Function with No Arguments
+#lambda function
+six = lambda : 6
+#call lambda function
+result = six()
+print(result)
